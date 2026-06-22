205fd7a3f789083125fc89faf4f9372869043c0c
Yibeltal Marie <yibeltalmarie1228@gmail.com>
2026-06-22 23:10:41 +0300

    to get current availabe gemini embedding models for you actual key

---
+++ list_embedding_models.py
@@ -0,0 +1,7 @@
+import os
+from google import genai
+
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+for model in client.models.list():
+    if "embedContent" in (model.supported_actions or []):
+        print(model.name)
